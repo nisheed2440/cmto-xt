9b514296b1631a06fce79bfdc7494c63233b7a0a
Nisheed Jagadish <nisjagad@publicisgroupe.net>
2018-06-26 18:06:58 +0530

    Added ripple effect

--- src/components/ScheduleTile.jsx
+++ src/components/ScheduleTile.jsx
@@ -7,6 +7,7 @@ import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
 import withWidth from "@material-ui/core/withWidth";
 import IconButton from "@material-ui/core/IconButton";
+import ButtonBase from "@material-ui/core/ButtonBase";
 import SessionTag from "./SessionTag";
 const styles = theme => ({
   root: {
@@ -23,7 +24,8 @@ const styles = theme => ({
     flex: 1,
     display: "flex",
     flexDirection: "column",
-    justifyContent: "center"
+    justifyContent: "center",
+    cursor: "pointer"
   },
   favButton: {},
   labelInline: {
@@ -36,7 +38,12 @@ const styles = theme => ({
     display: "block",
     position: "relative",
     color: "#000",
-    marginRight: '10px'
+    marginRight: "10px"
+  },
+  ripple: {
+    textAlign: 'inherit',
+    display:'block',
+    padding: theme.spacing.unit
   }
 });
 
@@ -48,19 +55,35 @@ class ScheduleTile extends Component {
         <CardContent className={classes.cardContent}>
           <div className={classes.timeSection} />
           <div className={classes.detailSection}>
-            <Typography variant="title" gutterBottom noWrap>
-              {title}
-            </Typography>
-            <div>
-              <Typography gutterBottom={width === 'xs'? true : false} variant="caption" className={width === 'xs'? classes.labelBlock : classes.labelInline}>
-                {venue}
+            <ButtonBase className={classes.ripple} onClick={() => (console.log('Clicked'))}>
+              <Typography variant="title" gutterBottom noWrap>
+                {title}
               </Typography>
-              {tags.map((tag) => (
-                <SessionTag key={tag.id} label={tag.label} color={tag.color || ''}/>
-              ))}
-            </div>
+              <div>
+                <Typography
+                  gutterBottom={width === "xs" ? true : false}
+                  variant="caption"
+                  className={
+                    width === "xs" ? classes.labelBlock : classes.labelInline
+                  }
+                >
+                  {venue}
+                </Typography>
+                {tags.map(tag => (
+                  <SessionTag
+                    key={tag.id}
+                    label={tag.label}
+                    color={tag.color || ""}
+                  />
+                ))}
+              </div>
+            </ButtonBase>
           </div>
-          <IconButton className={classes.favButton} aria-label="Favourite" onClick={favClick}>
+          <IconButton
+            className={classes.favButton}
+            aria-label="Favourite"
+            onClick={favClick}
+          >
             <Icon>star</Icon>
           </IconButton>
         </CardContent>
@@ -75,12 +98,12 @@ ScheduleTile.propTypes = {
   venue: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   favClick: PropTypes.func,
-  tags: PropTypes.array,
+  tags: PropTypes.array
 };
 
 ScheduleTile.defaultProps = {
   favClick: () => {},
   tags: []
-}
+};
 
 export default withStyles(styles)(withWidth()(ScheduleTile));
